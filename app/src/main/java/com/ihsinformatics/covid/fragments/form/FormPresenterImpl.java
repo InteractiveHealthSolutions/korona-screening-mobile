55129546bdbff8ef7889c6f6b19ed08502861e36
Nabeel Khowaja <nabeel.khowaja@ihsinformatics.com>
2020-03-13 19:05:08 +0500

    Layout changes in main screen added sindhi and urdu translations.

--- app/src/main/java/com/ihsinformatics/covid/fragments/form/FormPresenterImpl.java
+++ app/src/main/java/com/ihsinformatics/covid/fragments/form/FormPresenterImpl.java
@@ -110,17 +110,17 @@ public class FormPresenterImpl implements FormContract.Presenter {
     public List<Question> getQuestions() {
         List<Question> questions = new ArrayList<>();
 
-        questions.add(new Question(1, "Exposure", "recent_travel", view.getStringResource(R.string.sec_1_question_1), Arrays.asList(new Option(2, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_plane));
-        questions.add(new Question(1, "Exposure", "contact_traveller", view.getStringResource(R.string.sec_1_question_2), Arrays.asList(new Option(2, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_1b));
-        questions.add(new Question(1, "Exposure", "contact_patient", view.getStringResource(R.string.sec_1_question_3), Arrays.asList(new Option(2, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_1c));
-        questions.add(new Question(1, "Exposure", "household_member_patient", view.getStringResource(R.string.sec_1_question_4), Arrays.asList(new Option(2, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_1d));
+        questions.add(new Question(1, view.getStringResource(R.string.question_title_exposure), "recent_travel", view.getStringResource(R.string.sec_1_question_1), Arrays.asList(new Option(2, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_plane));
+        questions.add(new Question(1, view.getStringResource(R.string.question_title_exposure), "contact_traveller", view.getStringResource(R.string.sec_1_question_2), Arrays.asList(new Option(2, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_1b));
+        questions.add(new Question(1, view.getStringResource(R.string.question_title_exposure), "contact_patient", view.getStringResource(R.string.sec_1_question_3), Arrays.asList(new Option(2, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_1c));
+        questions.add(new Question(1, view.getStringResource(R.string.question_title_exposure), "household_member_patient", view.getStringResource(R.string.sec_1_question_4), Arrays.asList(new Option(2, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_1d));
 
-        questions.add(new Question(2, "Symptoms", "fever", view.getStringResource(R.string.sec_2_question_1), Arrays.asList(new Option(1, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_fever));
+        questions.add(new Question(2, view.getStringResource(R.string.question_title_symptoms), "fever", view.getStringResource(R.string.sec_2_question_1), Arrays.asList(new Option(1, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_fever));
 
-        questions.add(new Question(3, "Symptoms", "cough", view.getStringResource(R.string.sec_3_question_1), Arrays.asList(new Option(1, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_3a));
-        questions.add(new Question(3, "Symptoms", "cold", view.getStringResource(R.string.sec_3_question_2), Arrays.asList(new Option(1, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_cold));
-        questions.add(new Question(3, "Symptoms", "sore_throat", view.getStringResource(R.string.sec_3_question_3), Arrays.asList(new Option(1, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_sore_throat));
-        questions.add(new Question(3, "Symptoms", "sob", view.getStringResource(R.string.sec_3_question_4), Arrays.asList(new Option(1, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_shortness_of_breath));
+        questions.add(new Question(3, view.getStringResource(R.string.question_title_symptoms), "cough", view.getStringResource(R.string.sec_3_question_1), Arrays.asList(new Option(1, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_3a));
+        questions.add(new Question(3, view.getStringResource(R.string.question_title_symptoms), "cold", view.getStringResource(R.string.sec_3_question_2), Arrays.asList(new Option(1, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_cold));
+        questions.add(new Question(3, view.getStringResource(R.string.question_title_symptoms), "sore_throat", view.getStringResource(R.string.sec_3_question_3), Arrays.asList(new Option(1, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_sore_throat));
+        questions.add(new Question(3, view.getStringResource(R.string.question_title_symptoms), "sob", view.getStringResource(R.string.sec_3_question_4), Arrays.asList(new Option(1, Option.OptionValue.YES), new Option(0, Option.OptionValue.NO)), R.drawable.ic_shortness_of_breath));
 
         return questions;
     }
